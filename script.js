27b0ecbd955e6a95349bf7b26ad35517c9950b2e
Addyban <100277869+Addyban@users.noreply.github.com>
2023-05-06 23:35:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!', 'UI Designer!', 'Java Programmer!', 'C++ Programmer!',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
